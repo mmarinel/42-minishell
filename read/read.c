7698086f2e7103722932e15533e3a62a923076d4
Matteo Marinelli <mmarinel@e3r9p5.42roma.it>
2022-06-19 20:23:03 +0200

    history: fixato overlap entry  nella navigaione

--- read/read.c
+++ read/read.c
@@ -6,19 +6,27 @@
 /*   By: mmarinel <mmarinel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 16:51:54 by mmarinel          #+#    #+#             */
-/*   Updated: 2022/06/17 16:55:06 by mmarinel         ###   ########.fr       */
+/*   Updated: 2022/06/19 20:16:13 by mmarinel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "read.h"
-# include "prompt/prompt.h"
+#include "prompt/prompt.h"
 
 // TODO :-> put error where exit_shell is (put error will also use exit_shell)
 char	*shell_read( char *const envp[])
 {
 	char	*command;
 
-	command = ft_readline(get_current_working_directory(), e_true);
+	printf("\1");
+	command = ft_readline
+		(
+			ft_strjoin(
+				get_current_working_directory(),
+				ft_strjoin(RESET, "\2", e_false, e_false),
+				e_true, e_true),
+			e_true
+			);
 	printf("line read: %s\n", command);
 	return (command);
 	if (envp)
