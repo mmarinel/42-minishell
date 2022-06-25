e8d2f20112c5fa7571a8d15cef9d8e7ecc194dc4
Matteo Marinelli <mmarinel@e3r9p5.42roma.it>
2022-06-25 11:41:00 +0200

    parser: aggiunto modello dati

---
+++ read/parser/parser_types.h
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_types.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmarinel <mmarinel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/25 10:48:27 by mmarinel          #+#    #+#             */
+/*   Updated: 2022/06/25 11:40:25 by mmarinel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSER_TYPES_H
+# define PARSER_TYPES_H
+
+# define SIMPL_CMD 0
+# define ENV_DCL 1
+# define OPERATOR 2
+
+typedef enum e_operator
+{
+	e_PIPE,
+	e_OR,
+	e_AND,
+	e_PARENTH
+}	t_operator;
+
+typedef struct s_operator_node
+{
+	t_operator	operator;
+}	t_operator_node;
+
+typedef struct s_simple_command_node
+{
+	char	*cmd_name;
+	char	*cmd_args;
+}	t_simple_command_node;
+
+typedef struct s_bindings
+{
+	t_bool				concat;
+	char				*var_name;
+	char				*var_val;
+	struct s_bindings	*next;
+}	t_bindings;
+
+typedef struct s_env_decl_node
+{
+	t_bool		set;
+	t_bindings	*bindings;
+}	t_env_decl_node;
+
+typedef	struct s_node_content
+{
+	char					*infile;
+	char					*outfile;
+	short					content_type;
+	t_simple_command_node	simple_cmd;
+	t_env_decl_node			env_decl;
+	t_operator_node			operator_node;
+}	t_node_content;
+
+typedef struct s_tree_node
+{
+	t_node_content		content;
+	struct s_tree_node	*left;
+	struct s_tree_node	*right;
+}	t_tree_node;
+
+#endif
