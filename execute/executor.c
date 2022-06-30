fd056c17858f6356c68ca7004cf899f0b3d2c20f
Matteo Marinelli <mmarinel@e3r9p5.42roma.it>
2022-06-30 16:49:48 +0200

    executor: refactoring in progress

--- execute/executor.c
+++ execute/executor.c
@@ -6,7 +6,7 @@
 /*   By: mmarinel <mmarinel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 10:46:16 by mmarinel          #+#    #+#             */
-/*   Updated: 2022/06/30 14:14:49 by mmarinel         ###   ########.fr       */
+/*   Updated: 2022/06/30 16:49:13 by mmarinel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,54 +41,134 @@ void	execute_rec(t_tree_node *root, int in, int out)
 			pipe(new_in_out[2]);
 			left_hand_side.branch = fork();
 			if (!left_hand_side.branch)
-				return (execute_rec(root->left, in, new_in_out[1]));
+			{
+				close(new_in_out[0]);
+				execute_rec(root->left, in, new_in_out[1]);
+			}
 			right_hand_side.branch = fork();
 			if (!right_hand_side.branch)
-				return (execute_rec(root->right, new_in_out[0], out));
-			return ;
-		}
-		if (root->content->operator_node.operator == e_OR)
-		{
-			left_hand_side.branch = fork();
-			if (!left_hand_side.branch)
-				return (execute_rec(root->left, in, out));
+			{
+				close(new_in_out[1]);
+				execute_rec(root->right, new_in_out[0], out);
+			}
+			close(new_in_out[0]);
+			close(new_in_out[1]);
 			waitpid(left_hand_side.branch, &(left_hand_side.exit_status), 0);
-			if (left_hand_side.exit_status == EXIT_SUCCESS)
-				return ;
-			right_hand_side.branch = fork();
-			if (!right_hand_side.branch)
-				return (execute_rec(root->right, in, out));
 			waitpid(right_hand_side.branch, &(right_hand_side.exit_status), 0);
-			return ;
+			if ((!WIFEXITED(left_hand_side.exit_status) || WEXITSTATUS(left_hand_side.exit_status))
+				|| (!WIFEXITED(right_hand_side.exit_status) || WEXITSTATUS(right_hand_side.exit_status)))
+				exit(EXIT_FAILURE);
+			exit(EXIT_SUCCESS);
 		}
 		if (root->content->operator_node.operator == e_AND)
 		{
+			pipe(new_in_out[2]);
 			left_hand_side.branch = fork();
 			if (!left_hand_side.branch)
-				return (execute_rec(root->left, in, out));
+			{
+				close(new_in_out[0]);
+				execute_rec(root->left, in, new_in_out[1]);
+			}
 			waitpid(left_hand_side.branch, &(left_hand_side.exit_status), 0);
-			if (left_hand_side.exit_status != EXIT_SUCCESS)
-				return ;
+			if ((!WIFEXITED(left_hand_side.exit_status) || WEXITSTATUS(left_hand_side.exit_status)))
+			{
+				close(new_in_out[0]);
+				close(new_in_out[1]);
+				exit(EXIT_FAILURE);
+			}
 			right_hand_side.branch = fork();
 			if (!right_hand_side.branch)
-				return (execute_rec(root->right, in, out));
+			{
+				close(new_in_out[1]);
+				execute_rec(root->right, new_in_out[0], out);
+			}
 			waitpid(right_hand_side.branch, &(right_hand_side.exit_status), 0);
-			return ;
+			close(new_in_out[0]);
+			close(new_in_out[1]);
+			if ((!WIFEXITED(right_hand_side.exit_status) || WEXITSTATUS(right_hand_side.exit_status)))
+				exit(EXIT_FAILURE);
+			exit(EXIT_SUCCESS);
 		}
-		// else
-		// {
-		// 	new_in_out[0] = in;
-		// 	new_in_out[1] = out;
-		// }
-		// left_hand_side = fork();
-		// if (!left_hand_side)
-		// {
-		// 	// TODO
-		// }
-		// right_hand_side = fork();
-		// if (!right_hand_side)
-		// {
-		// 	// TODO
-		// }
+		if (root->content->operator_node.operator == e_OR)
+		{
+			pipe(new_in_out[2]);
+			left_hand_side.branch = fork();
+			if (!left_hand_side.branch)
+			{
+				close(new_in_out[0]);
+				execute_rec(root->left, in, new_in_out[1]);
+			}
+			waitpid(left_hand_side.branch, &(left_hand_side.exit_status), 0);
+			if (WIFEXITED(left_hand_side.exit_status) && !WEXITSTATUS(left_hand_side.exit_status))
+			{
+				close(new_in_out[0]);
+				close(new_in_out[1]);
+				exit(EXIT_SUCCESS);
+			}
+			right_hand_side.branch = fork();
+			if (!right_hand_side.branch)
+			{
+				close(new_in_out[1]);
+				execute_rec(root->right, new_in_out[0], out);
+			}
+			waitpid(right_hand_side.branch, &(right_hand_side.exit_status), 0);
+			close(new_in_out[0]);
+			close(new_in_out[1]);
+			if ((!WIFEXITED(right_hand_side.exit_status) || WEXITSTATUS(right_hand_side.exit_status)))
+				exit(EXIT_FAILURE);
+			exit(EXIT_SUCCESS);
+		}
 	}
 }
+
+void	exit_execution(t_branch left_hand_side, t_branch right_hand_side, t_operator operator, int new_in_out[2])
+{
+	close(new_in_out[0]);
+	close(new_in_out[1]);
+	if (operator != e_PIPE)
+		exit(EXIT_SUCCESS);
+	else
+	{
+		waitpid(left_hand_side.branch, &(left_hand_side.exit_status), 0);
+		waitpid(right_hand_side.branch, &(right_hand_side.exit_status), 0);
+		if (
+			(
+				!WIFEXITED(left_hand_side.exit_status)
+				|| WEXITSTATUS(left_hand_side.exit_status)
+			)
+			||
+			(
+				!WIFEXITED(right_hand_side.exit_status)
+				|| WEXITSTATUS(right_hand_side.exit_status)
+			)
+		)
+			exit(EXIT_FAILURE);
+		exit(EXIT_SUCCESS);
+	}
+}
+
+t_branch	execute_branch(t_tree_node *new_root, t_operator operator,int old_in_out, int new_in_out[2])
+{
+	t_branch	branch_handle;
+
+	branch_handle.branch = fork();
+	if (!branch_handle.branch)
+	{
+		// TODO .....
+	}
+	else
+	{
+		// TODO ......
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
