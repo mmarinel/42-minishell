b5c49a9ab1918dbc66a1c52819f46c23e5ff0da7
Matteo Marinelli <mmarinel@e3r10p4.42roma.it>
2022-07-05 16:19:25 +0200

    env: normato

--- env/env.c
+++ env/env.c
@@ -6,18 +6,20 @@
 /*   By: mmarinel <mmarinel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 14:00:37 by mmarinel          #+#    #+#             */
-/*   Updated: 2022/07/05 15:45:29 by mmarinel         ###   ########.fr       */
+/*   Updated: 2022/07/05 16:18:27 by mmarinel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "env.h"
 
 static void	*env_return_cases(t_bindings *env, t_bindings *export,
-				t_env_handl_opcode opcode, void *argument);
-static void	*env_management_cases(t_bindings *env, t_bindings *export,
-				t_env_handl_opcode opcode, void *argument);
+					t_env_handl_opcode opcode);
+static void	*env_management_cases(t_bindings **env, t_bindings **export,
+					t_env_handl_opcode opcode, void *argument);
 static void	*env_operations_cases(t_bindings *env, t_bindings *export,
 				t_env_handl_opcode opcode, void *argument);
+static void	*debug_cases(t_bindings *env, t_bindings *export,
+				t_env_handl_opcode opcode, void *argument); // * debug
 
 void	*env_handler(t_env_handl_opcode opcode, void *argument)
 {
@@ -27,14 +29,66 @@ void	*env_handler(t_env_handl_opcode opcode, void *argument)
 	if (opcode == ENV_INITIALIZE
 		|| opcode == ENV_CLEAN)
 	{
-		return (env_management_cases(env, export, opcode, argument));
+		return (env_management_cases(&env, &export, opcode, argument));
 	}
 	if (opcode == ENV_RETURN
 		|| opcode == EXPORT_RETURN
 		|| opcode == ENV_LIST_TO_ARRAY)
 	{
-		return (env_return_cases(env, export, opcode, argument));
+		return (env_return_cases(env, export, opcode));
 	}
+	if (opcode == BINDING_UPDATE
+		|| opcode == BINDING_UNSET
+		|| opcode == BINDING_GET_VALUE)
+	{
+		return (env_operations_cases(env, export, opcode, argument));
+	}
+	if (opcode == _PRINT_ENV_)
+	{
+		return (debug_cases(env, export, opcode, argument));
+	}
+	return (NULL);
+}
+
+static void	*env_return_cases(t_bindings *env, t_bindings *export,
+					t_env_handl_opcode opcode)
+{
+	if (opcode == ENV_RETURN)
+	{
+		return (env);
+	}
+	if (opcode == EXPORT_RETURN)
+	{
+		return (export);
+	}
+	if (opcode == ENV_LIST_TO_ARRAY)
+	{
+		return ((void *)bindings_list_to_array(env));
+	}
+	return (NULL);
+}
+
+static void	*env_management_cases(t_bindings **env_ref, t_bindings **exprt_ref,
+					t_env_handl_opcode opcode, void *argument)
+{
+	if (opcode == ENV_INITIALIZE)
+	{
+		copy_env(env_ref, (char **)argument, e_false);
+		copy_env(exprt_ref, (char **)argument, e_true);
+	}
+	if (opcode == ENV_CLEAN)
+	{
+		free_env(*env_ref);
+		free_env(*exprt_ref);
+		*env_ref = NULL;
+		*exprt_ref = NULL;
+	}
+	return (NULL);
+}
+
+static void	*env_operations_cases(t_bindings *env, t_bindings *export,
+				t_env_handl_opcode opcode, void *argument)
+{
 	if (opcode == BINDING_UPDATE)
 	{
 		if (e_true == binding_exist(env, argument))
@@ -55,57 +109,27 @@ void	*env_handler(t_env_handl_opcode opcode, void *argument)
 	}
 	if (opcode == BINDING_GET_VALUE)
 	{
-		return (binding_get_value(env, argument))
-	}
-	if (opcode == _PRINT_ENV_)
-	{
-		t_bindings	*bindings;
-		bindings = export;
-		while (bindings)
-		{
-			printf("%s=%s\n", bindings->var_name, bindings->var_val);
-			bindings =  bindings->next;
-		}
-		exit(0);
+		return (binding_get_value(env, argument));
 	}
 	return (NULL);
 }
 
-static *void	env_return_cases(t_bindings *env, t_bindings *export,
+static void	*debug_cases(t_bindings *env, t_bindings *export,
 				t_env_handl_opcode opcode, void *argument)
 {
-	if (opcode == ENV_RETURN)
-	{
-		return (env);
-	}
-	if (opcode == EXPORT_RETURN)
-	{
-		return (export);
-	}
-	if (opcode == ENV_LIST_TO_ARRAY)
-	{
-		return ((void *)bindings_list_to_array(env));
-	}
-}
+	t_bindings	*current;
 
-static *void	env_management_cases(t_bindings *env, t_bindings *export,
-					t_env_handl_opcode opcode, void *argument)
-{
-	if (opcode == ENV_INITIALIZE)
+	if (env && export && argument)
+		;
+	if (opcode == _PRINT_ENV_)
 	{
-		copy_env(&env, (char **)argument, e_false);
-		copy_env(&export, (char **)argument, e_true);
+		current = export;
+		while (current)
+		{
+			printf("%s=%s\n", current->var_name, current->var_val);
+			current = current->next;
+		}
+		// exit(0);
 	}
-	if (opcode == ENV_CLEAN)
-	{
-		free_env(env);
-		free_env(export);
-		env = NULL;
-		export = NULL;
-	}
-}
-
-static void	*env_operations_cases(t_bindings *env, t_bindings *export,
-				t_env_handl_opcode opcode, void *argument)
-{
+	return (NULL);
 }
