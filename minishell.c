583dbc7b9ac75bf53a5cf90601db9336b67ea59e
evento evento <evento@e3r8p1.42roma.it>
2022-07-10 16:35:00 +0200

    risolto incremento SHLVL ogni volta che apro minishell

--- minishell.c
+++ minishell.c
@@ -12,59 +12,56 @@
 
 #include "minishell.h"
 
+static void	set_env(char *const envp[]);
 static void	set_sig_handlers(void);
 static void	print_signature(void);
 static void	unlink_here_docs(void);
 
 int	main(int argc, char const *argv[], char *const envp[])
 {
-	// char			*command;
 	t_tree_node		*parse_tree;
 
 	if (argc != 1)
-		return (EXIT_SUCCESS);
-	// g_shell_env = (t_shell_env *) malloc(sizeof(t_shell_env));
-	env_handler(ENV_INITIALIZE, (char **)envp);
-	env_handler(BINDING_UPDATE, get_new_binding("SHLVL", "2", e_false));
-	// env_handler(_PRINT_ENV_, NULL);
-	g_env.last_executed_cmd_exit_status = EXIT_SUCCESS;
-	// g_env.env = NULL;
-	// g_env.export = NULL;
-	// copy_env(&(g_env.env), (char **)envp, e_false);
-	// copy_env(&(g_env.export), (char **)envp, e_true);
-	// over_write_binding(g_env.env, get_new_binding("SHLVL", "2", e_false));
-	// over_write_binding(g_env.export, get_new_binding("SHLVL", "2", e_false));
-	// t_bindings	*bindings;
-	// bindings = g_env.export;
-	// while (bindings)
-	// {
-	// 	printf("%s=%s\n", bindings->var_name, bindings->var_val);
-	// 	bindings =  bindings->next;
-	// }
-	// exit(0);
-	set_sig_handlers();
+	{
+		printf(RED "no arguments allowed\n" RESET
+		CYAN "found argument: " RESET
+		YELLOW"%s\n" RESET,
+		*(argv + 1));
+		exit(EXIT_FAILURE);
+	}
+	set_env(envp);
+	set_sig_handlers(); // * rename to sig-handling since we also handle ctrl + c and ctrl + d display here!!!!
 	print_signature();
 	while (e_true)
 	{
 		parse_tree = shell_read();
-		// if (g_env.last_executed_cmd_exit_status == EXIT_SUCCESS)
 		execute(parse_tree);
-		// [result, fd] = shell_evaluate(command) ....return struct
-		// shell_print(fd, result) .....use write(fd, ...)
-		// ft_free(command);
 		tokenizer_free();
 		free_tree(&parse_tree);
-		unlink_here_docs();// * DEBUG: COMMENTED BECAUSE WE MAY NEED TO SEE WHAT'S INSIDE OF THE FILE
+		unlink_here_docs();
 	}
 	clear_history();
 	return (EXIT_SUCCESS);
-	if (argv)
-	{}
+}
+
+/**
+ * @brief this function saves the current env after incrementing variable SHLVL
+ * 
+ * @param envp 
+ */
+static void	set_env(char *const envp[])
+{
+	size_t	cur_shlvl;
+
+	env_handler(ENV_INITIALIZE, (char **)envp);
+	cur_shlvl = ft_atoi(env_handler(BINDING_GET_VALUE, "SHLVL"));
+	env_handler(BINDING_UPDATE, get_new_binding("SHLVL", ft_itoa(cur_shlvl + 1), e_false));
+	g_env.last_executed_cmd_exit_status = EXIT_SUCCESS;
 }
 
 /**
  * @brief this function sets signals handlers
- * and disable echoing of special character ctr+c (^C)
+ * and disable echoing of special characters -ctr+c (^C), ...
  * 
  */
 static void	set_sig_handlers(void)
@@ -79,6 +76,10 @@ static void	set_sig_handlers(void)
 	signal(SIGQUIT, SIG_IGN);
 }
 
+/**
+ * @brief this function removes here docs buffer files from the file system
+ * 
+ */
 static void	unlink_here_docs(void)
 {
 	char	*prefix;
